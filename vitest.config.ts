ec4cf48a5843a533bf26bb176372b51c911b1592
Vu Khoi <khoidragonslayer@gmail.com>
2025-11-01 12:02:45 +0700

    feat: setup project

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,19 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  plugins: [react()],
   test: {
+    globals: true,
     environment: 'jsdom',
-    include: ['**/*.test.tsx'],
+    setupFiles: './src/test/setup.ts',
+    include: ['**/*.test.tsx', '**/*.test.ts'],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
-})
+});
